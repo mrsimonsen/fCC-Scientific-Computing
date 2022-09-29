d8addbd365baacc649182132cac125753b859ce5
mrsimonsen <johnsimonsen@weber.edu>
2022-09-29 12:24:06 -0600

    mean_var_std complete

--- Data Analysis with Python/mean-variance-standard-deviation-calculator/mean_var_std.py
+++ Data Analysis with Python/mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -4,16 +4,22 @@ def calculate(list):
 	#convert list of 9 digits to 3x3 matrix
 	if len(list) < 9:
 		raise ValueError("List must contain nine numbers.")
-	#data = np.array([[list]])
+	data = np.array([
+		list[:3],
+		list[3:6],
+		list[6:]
+	])
+
 	#return a dictionary of calculated values
 	#format: axis1, axis2, flattened
+	#entries should be normal python lists
 	calculations = {
-		'mean': [],
-		'variance': [],
-		'standard deviation': [],
-		'max': [],
-		'min': [],
-		'sum': []
+		'mean': [np.mean(data,axis=0).tolist(), np.mean(data,axis=1).tolist(), np.mean(data)],
+		'variance': [np.var(data,axis=0).tolist(), np.var(data,axis=1).tolist(), np.var(data)],
+		'standard deviation': [np.std(data,axis=0).tolist(), np.std(data,axis=1).tolist(), np.std(data)],
+		'max': [np.amax(data,axis=0).tolist(), np.amax(data,axis=1).tolist(), np.amax(data)],
+		'min': [np.min(data,axis=0).tolist(), np.min(data,axis=1).tolist(), np.min(data)],
+		'sum': [np.sum(data,axis=0).tolist(), np.sum(data,axis=1).tolist(), np.sum(data)]
 	}
 	
 	return calculations
