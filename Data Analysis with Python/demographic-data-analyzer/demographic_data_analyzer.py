809a7a12deb9e6556f4d729ebf555f0ddce2143d
mrsimonsen <johnsimonsen@weber.edu>
2022-09-30 08:31:25 -0600

    7th

--- Data Analysis with Python/demographic-data-analyzer/demographic_data_analyzer.py
+++ Data Analysis with Python/demographic-data-analyzer/demographic_data_analyzer.py
@@ -29,9 +29,9 @@ def calculate_demographic_data(print_data=True):
 	min_work_hours = df['hours-per-week'].min()
 
 	# What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-	num_min_workers = None
+	num_min_workers = df.loc[df['hours-per-week']==min_work_hours].shape[0]
 
-	rich_percentage = None
+	rich_percentage = round(df.loc[(df['hours-per-week']==min_work_hours) & (df['salary']=='>50K')].shape[0]/num_min_workers*100,1)
 
 	# What country has the highest percentage of people that earn >50K?
 	highest_earning_country = None
