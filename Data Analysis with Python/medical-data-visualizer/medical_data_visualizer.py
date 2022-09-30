aa39c685804252817c2dd4ce05317b0eb2a90853
mrsimonsen <johnsimonsen@weber.edu>
2022-09-30 12:35:14 -0600

    working on step3 - line 19 and spaces to tabs

--- Data Analysis with Python/medical-data-visualizer/medical_data_visualizer.py
+++ Data Analysis with Python/medical-data-visualizer/medical_data_visualizer.py
@@ -15,47 +15,46 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+	# Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+	df_cat = df.melt(id_vars=['cardio'],value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+
+	# Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+	df_cat = None
+	
+
+	# Draw the catplot with 'sns.catplot()'
 
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
 
-    # Draw the catplot with 'sns.catplot()'
+	# Get the figure for the output
+	fig = None
 
 
-
-    # Get the figure for the output
-    fig = None
-
-
-    # Do not modify the next two lines
-    fig.savefig('catplot.png')
-    return fig
+	# Do not modify the next two lines
+	fig.savefig('catplot.png')
+	return fig
 
 
 # Draw Heat Map
 def draw_heat_map():
-    # Clean the data
-    df_heat = None
+	# Clean the data
+	df_heat = None
 
-    # Calculate the correlation matrix
-    corr = None
+	# Calculate the correlation matrix
+	corr = None
 
-    # Generate a mask for the upper triangle
-    mask = None
-
-
-
-    # Set up the matplotlib figure
-    fig, ax = None
-
-    # Draw the heatmap with 'sns.heatmap()'
+	# Generate a mask for the upper triangle
+	mask = None
 
 
 
-    # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+	# Set up the matplotlib figure
+	fig, ax = None
+
+	# Draw the heatmap with 'sns.heatmap()'
+
+
+
+	# Do not modify the next two lines
+	fig.savefig('heatmap.png')
+	return fig
