f70b73970b5b9ad3891e9f3481908de8d15da13b
mrsimonsen <johnsimonsen@weber.edu>
2022-10-04 10:47:19 -0600

    2nd part done

--- Data Analysis with Python/page-view-time-series-visualizer/time_series_visualizer.py
+++ Data Analysis with Python/page-view-time-series-visualizer/time_series_visualizer.py
@@ -8,7 +8,7 @@ register_matplotlib_converters()
 df = pd.read_csv("fcc-forum-pageviews.csv", index_col='date', parse_dates=True)
 
 # Clean data
-df = None
+df = df[(df['value']>=df['value'].quantile(0.025))&(df['value']<=df['value'].quantile(0.975))]
 
 
 def draw_line_plot():
