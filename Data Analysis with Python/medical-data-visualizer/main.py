6f475e442b553761b715052084a84d4ce3204574
mrsimonsen <johnsimonsen@weber.edu>
2022-10-04 09:14:46 -0600

    modified tests to remove 'id' field from heatmap, becasue there is no reason for patient id to correlated with health values

--- Data Analysis with Python/medical-data-visualizer/main.py
+++ Data Analysis with Python/medical-data-visualizer/main.py
@@ -3,10 +3,8 @@ import medical_data_visualizer
 from unittest import main
 
 # Test your function by calling it here
-#medical_data_visualizer.draw_cat_plot()
-#medical_data_visualizer.draw_heat_map()
+medical_data_visualizer.draw_cat_plot()
+medical_data_visualizer.draw_heat_map()
 
 # Run unit tests automatically
-#main(module='test_module', exit=False)
-from os import system
-system("python3 test_module.py HeatMapTestCase.test_heat_map_labels")
+main(module='test_module', exit=False)
