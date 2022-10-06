cd362e4cacff9e306f86e364c1e449fac3e80df3
mrsimonsen <johnsimonsen@weber.edu>
2022-10-06 09:32:22 -0600

    spaces to tabs

--- Data Analysis with Python/page-view-time-series-visualizer/time_series_visualizer.py
+++ Data Analysis with Python/page-view-time-series-visualizer/time_series_visualizer.py
@@ -12,44 +12,44 @@ df = df[(df['value']>=df['value'].quantile(0.025))&(df['value']<=df['value'].qua
 
 
 def draw_line_plot():
-    # Draw line plot
+	# Draw line plot
 	title = "Daily freeCodeCamp Forum Page Views 5/2016-12/2019"
 	x = "Date"
 	y = "Page Views"
 	
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
-    return fig
+	# Save image and return fig (don't change this part)
+	fig.savefig('line_plot.png')
+	return fig
 
 def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
+	# Copy and modify data for monthly bar plot
+	df_bar = None
 
-    # Draw bar plot
-
-
+	# Draw bar plot
 
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
-    return fig
+
+
+	# Save image and return fig (don't change this part)
+	fig.savefig('bar_plot.png')
+	return fig
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
-    df_box = df.copy()
-    df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+	# Prepare data for box plots (this part is done!)
+	df_box = df.copy()
+	df_box.reset_index(inplace=True)
+	df_box['year'] = [d.year for d in df_box.date]
+	df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
-    # Draw box plots (using Seaborn)
-
-
+	# Draw box plots (using Seaborn)
 
 
 
-    # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+
+
+	# Save image and return fig (don't change this part)
+	fig.savefig('box_plot.png')
+	return fig
