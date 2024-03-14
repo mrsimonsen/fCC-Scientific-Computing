21f03bda32253b6de1201f04f220b8b7b1f858c6
John Simonsen <simobear14@gmail.com>
2024-03-14 16:27:05 -0600

    saving for dinner

--- Data Analysis with Python/sea-level-predictor/sea_level_predictor.py
+++ Data Analysis with Python/sea-level-predictor/sea_level_predictor.py
@@ -4,10 +4,10 @@ from scipy.stats import linregress
 
 def draw_plot():
 	# Read data from file
-
+	df = pd.read_csv('epa-sea-level.csv')
 
 	# Create scatter plot
-
+	
 
 	# Create first line of best fit
 
