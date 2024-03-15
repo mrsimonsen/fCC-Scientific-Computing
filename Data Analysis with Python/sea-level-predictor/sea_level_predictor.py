6d38f580a9159d218c579fd1b39d8db21fa31b5f
mrsimonsen <simobear14@gmail.com>
2024-03-15 14:15:22 -0600

    all tests passed

--- Data Analysis with Python/sea-level-predictor/sea_level_predictor.py
+++ Data Analysis with Python/sea-level-predictor/sea_level_predictor.py
@@ -24,7 +24,9 @@ def draw_plot():
 	plt.plot(recent_years, recent_predictions, color='blue')
 
 	# Add labels and title
-
+	plt.xlabel("Year")
+	plt.ylabel("Sea Level (inches)")
+	plt.title("Rise in Sea Level")
 
 	# Save plot and return data for testing (DO NOT MODIFY)
 	plt.savefig('sea_level_plot.png')
