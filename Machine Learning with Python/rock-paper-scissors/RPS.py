4afd77debcb08e4afcb9aa56e70cf5c94e98d326
John Simonsen <simobear14@gmail.com>
2024-04-08 16:14:44 -0600

    last was mrugesh & quincy, this is kris & quincy

--- Machine Learning with Python/rock-paper-scissors/RPS.py
+++ Machine Learning with Python/rock-paper-scissors/RPS.py
@@ -10,13 +10,13 @@ def player(prev_play, opponent_history=[]):
 	#if the opponent has played the same move twice
 	if len(opponent_history) > 2 and opponent_history[-2] == prev_play:
 		if prev_play == "R":
-			guess = "P"
-		elif prev_play == "P":
 			guess = "S"
-		else:
+		elif prev_play == "P":
 			guess = "R"
+		else:
+			guess = "P"
 	elif len(opponent_history) > 1:
-		guess = win_move[prev_play]
+		guess = prev_play
 	else:
 		guess = choice(list(win_move.keys()))
 
--- Machine Learning with Python/rock-paper-scissors/main.py
+++ Machine Learning with Python/rock-paper-scissors/main.py
@@ -3,17 +3,13 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-print('quincy')
 play(player, quincy, 1000)
-print('abbey')
 play(player, abbey, 1000)
-print('kris')
 play(player, kris, 1000)
-print('mrugesh')
 play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
-# play(human, abbey, 20, verbose=True)
+#play(human, abbey, 20, verbose=True)
 
 # Uncomment line below to play against a bot that plays randomly:
 # play(human, random_player, 1000)
