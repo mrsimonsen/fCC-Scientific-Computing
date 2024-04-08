bdd5bfc7c8802a09b78be8d488b0c607ce40d3f7
John Simonsen <simobear14@gmail.com>
2024-04-08 17:38:52 -0600

    saving for dinner

--- Machine Learning with Python/rock-paper-scissors/RPS.py
+++ Machine Learning with Python/rock-paper-scissors/RPS.py
@@ -7,17 +7,10 @@ def player(prev_play, opponent_history=[]):
 	if prev_play == '':
 		opponent_history = ['']
 
-	#if the opponent has played the same move twice
-	if len(opponent_history) > 2 and opponent_history[-2] == prev_play:
-		if prev_play == "R":
-			guess = "S"
-		elif prev_play == "P":
-			guess = "R"
-		else:
-			guess = "P"
-	elif len(opponent_history) > 1:
-		guess = prev_play
+	#this beats mrugesh & quincy
+	if len(opponent_history) > 1:
+		guess = win_move[prev_play]
 	else:
-		guess = choice(list(win_move.keys()))
+		guess = "P"
 
 	return guess
