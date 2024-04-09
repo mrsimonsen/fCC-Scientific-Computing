d4b12eab56d885d228575e5fb1031c0502a7be74
John Simonsen <simobear14@gmail.com>
2024-04-09 12:28:15 -0600

    beat all 4 bots at the same time

--- Machine Learning with Python/rock-paper-scissors/RPS.py
+++ Machine Learning with Python/rock-paper-scissors/RPS.py
@@ -7,10 +7,20 @@ def player(prev_play, opponent_history=[]):
 	if prev_play == '':
 		opponent_history = ['']
 
+	
+	if len(opponent_history)%5 == 0:
+		random = ""
+		for move in "RPS":
+			if move != prev_play:
+				random += move
+		guess = choice(move)
+	#this beats abbey
+	elif len(opponent_history) > 3 and prev_play == opponent_history[-2] and prev_play == opponent_history[-3]:
+		guess = prev_play
 	#this beats mrugesh & quincy
-	if len(opponent_history) > 1:
+	elif len(opponent_history) > 1:
 		guess = win_move[prev_play]
 	else:
-		guess = "P"
+		guess = "R"
 
 	return guess
